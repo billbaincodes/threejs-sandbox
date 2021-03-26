d4913cf94767648a8fde71e547573fed881842c8
bill <williamandrewbain@gmail.com>
2021-03-26 17:24:35 -0600

    clean up js

--- script.js
+++ script.js
@@ -1,92 +1,83 @@
-console.log('become one with inner selfness')
+console.log("become one with inner selfness");
 
 // import './style.css'
-import * as THREE from '../node_modules/three/build/three.module.js'
+import * as THREE from "../node_modules/three/build/three.module.js";
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from '../node_modules/dat.gui/build/dat.gui.module.js'
+import * as dat from "../node_modules/dat.gui/build/dat.gui.module.js";
 
-// // Debug
-const gui = new dat.GUI()
+// Debug
+const gui = new dat.GUI();
 
-// // Canvas
-const canvas = document.querySelector('canvas.webgl')
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
 
-// // Scene
-const scene = new THREE.Scene()
+// Scene
+const scene = new THREE.Scene();
 
-// // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+// Objects
+const geometry = new THREE.TorusGeometry(0.7, 0.2, 16, 100);
 
-// // Materials
+// Materials
+const material = new THREE.MeshBasicMaterial();
+material.color = new THREE.Color(0xff0000);
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+// Mesh
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
-// // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+// Lights
+const pointLight = new THREE.PointLight(0xffffff, 0.1);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
 
-// // Lights
-
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
-
-// /**
-//  * Sizes
-//  */
+// Sizes
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-/**
- * Camera
- */
+// Camera
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 2;
+scene.add(camera);
 
 // Controls
 // const controls = new OrbitControls(camera, canvas)
 // controls.enableDamping = true
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-/**
- * Animate
- */
+// Animate
+const clock = new THREE.Clock();
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-const clock = new THREE.Clock()
+  // Update objects
+  sphere.rotation.y = 0.5 * elapsedTime;
 
-const tick = () =>
-{
+  // Update Orbital Controls
+  // controls.update()
 
-    const elapsedTime = clock.getElapsedTime()
+  // Render
+  renderer.render(scene, camera);
 
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-    // Update Orbital Controls
-    // controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+tick();
